523f2650f581f8c36bcd81fd6d23b5a0800bc718
Harvey <harveywyatt@hotmail.com>
2020-09-23 19:35:50 +0930

    Change Main use semantics

--- src/main/java/me/raddari/chip8/Main.java
+++ src/main/java/me/raddari/chip8/Main.java
@@ -1,9 +1,19 @@
 package me.raddari.chip8;
 
+import me.raddari.chip8.disassembler.Disassembler;
+import me.raddari.chip8.disassembler.Opcode;
+
+import java.io.File;
+
 public final class Main {
 
     public static void main(String[] args) {
-
+        var romFile = new File(Main.class.getResource("rom/RandomNumberTest.ch8").getPath());
+        Disassembler.create()
+                .disassemble(romFile)
+                .stream()
+                .map(Opcode::toAssemblyString)
+                .forEach(System.out::println);
     }
 
 }
