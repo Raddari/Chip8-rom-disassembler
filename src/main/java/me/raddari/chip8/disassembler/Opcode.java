ed40488d8d105fa5750cb1f85de6af266492e59d
Harvey <harveywyatt@hotmail.com>
2020-09-23 19:42:12 +0930

    Add getters and hashcode overrides to Opcode

--- src/main/java/me/raddari/chip8/disassembler/Opcode.java
+++ src/main/java/me/raddari/chip8/disassembler/Opcode.java
@@ -1,8 +1,10 @@
 package me.raddari.chip8.disassembler;
 
+import com.google.common.base.MoreObjects;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
+import java.util.Objects;
 
 public final class Opcode {
 
@@ -14,8 +16,46 @@ public final class Opcode {
         this.args = args;
     }
 
+    public static @NotNull Opcode create(@NotNull Kind kind, @NotNull List<Argument> args) {
+        return new Opcode(kind, args);
+    }
+
     public @NotNull String toAssemblyString() {
         return "";
+    }
+
+    public @NotNull Kind getKind() {
+        return kind;
+    }
+
+    public @NotNull List<Argument> getArgs() {
+        return List.copyOf(args);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        final Opcode opcode = (Opcode) o;
+        return kind == opcode.kind &&
+                args.equals(opcode.args);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(kind, args);
+    }
+
+    @Override
+    public String toString() {
+        return MoreObjects.toStringHelper(this)
+                .add("kind", kind)
+                .add("args", args)
+                .toString();
     }
 
     public enum Kind {
