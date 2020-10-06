deff17efa890e4228f4e80ce4745afbd42fda27f
Harvey <harveywyatt@hotmail.com>
2020-10-07 02:40:00 +1030

    Update semantics to new file formatting system

--- src/main/java/me/raddari/chip8/Main.java
+++ src/main/java/me/raddari/chip8/Main.java
@@ -1,10 +1,13 @@
 package me.raddari.chip8;
 
+import me.raddari.chip8.config.JsonConfiguration;
 import me.raddari.chip8.disassembler.Disassembler;
+import me.raddari.chip8.format.AssemblyFileFormatter;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.io.File;
+import java.io.IOException;
 
 public final class Main {
 
@@ -16,17 +19,28 @@ public final class Main {
             return;
         }
 
-        var romFile = new File(args[0]);
-        if (romFile.isFile()) {
-            var program = Disassembler.create().disassemble(romFile);
-            System.out.println(program.name());
-            for (var line : program.opcodes()) {
-                System.out.println(line.toAssemblyString());
+        var formatConfigFile = new File(
+                Main.class.getClassLoader().getResource("assets/format/default.json").getPath());
+        try {
+            var config = new JsonConfiguration().load(formatConfigFile);
+            var formatter = new AssemblyFileFormatter(config);
+            var romFile = new File(args[0]);
+
+            if (romFile.isFile()) {
+                var program = Disassembler.create().disassemble(romFile);
+                var outFile = new File(romFile.getName() + ".asm");
+                formatter.formatToFile(program, outFile);
+
+            } else {
+                LOGGER.fatal("Argument {} must point to a rom file", romFile.getAbsolutePath());
+                printHelp();
             }
-        } else {
-            LOGGER.fatal("Argument {} must point to a rom file", romFile.getAbsolutePath());
-            printHelp();
+
+        } catch (IOException e) {
+            LOGGER.error("IOException occured disassembling file", e);
         }
+
+
     }
 
     private static void printHelp() {
