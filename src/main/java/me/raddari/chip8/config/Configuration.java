c89680b9e8fa28de82eb4f17f6a7b724493c03db
Harvey <harveywyatt@hotmail.com>
2020-10-08 19:56:14 +1030

    Update javadoc

--- src/main/java/me/raddari/chip8/config/Configuration.java
+++ src/main/java/me/raddari/chip8/config/Configuration.java
@@ -3,8 +3,29 @@ package me.raddari.chip8.config;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+/**
+ * <p>Defines the common methods for a configuration which is readable by this program. A configuration is very similar
+ * to a {@link java.util.Map}, to the point where most implementations can use a {@code Map} as the underlying data
+ * structure.
+ * <p>All methods in a configuration must not accept or return {@code null}, EXCEPT FOR
+ * <ul>
+ *   <li>
+ *     {@link #get(String)} and its overloads, which MAY return {@code null} or receive {@code null} as its
+ *     default parameter.
+ *   </li>
+ *   <li>
+ *     {@link #set(String, Object)} which MAY receive {@code null} as its object parameter, to remove a config option.
+ *   </li>
+ * </ul>
+ */
 public interface Configuration {
 
+    /**
+     * Gets the value associated to the given path.
+     *
+     * @param path config value path
+     * @return {@code Object} associated with the given path, or {@code null} if it does not exist.
+     */
     @Nullable Object get(@NotNull String path);
 
     @Nullable Object get(@NotNull String path, @Nullable Object def);
