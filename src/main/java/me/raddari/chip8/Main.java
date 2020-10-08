cba9caddc9a48bff2a91624a6f754bbb0d8eead0
Harvey <harveywyatt@hotmail.com>
2020-10-08 18:05:31 +1030

    Add ability to use a custom config

--- src/main/java/me/raddari/chip8/Main.java
+++ src/main/java/me/raddari/chip8/Main.java
@@ -1,15 +1,13 @@
 package me.raddari.chip8;
 
+import me.raddari.chip8.config.Configuration;
 import me.raddari.chip8.config.JsonConfiguration;
 import me.raddari.chip8.disassembler.Disassembler;
 import me.raddari.chip8.format.AssemblyFileFormatter;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public final class Main {
 
@@ -22,9 +20,7 @@ public final class Main {
         }
 
         try {
-            var configStream = Main.class.getResourceAsStream("/assets/format/default.json");
-            var configReader = new BufferedReader(new InputStreamReader(configStream));
-            var config = new JsonConfiguration().load(configReader);
+            var config = getCustomConfiguration();
             var formatter = new AssemblyFileFormatter(config);
             var romFile = new File(args[0]);
 
@@ -41,8 +37,26 @@ public final class Main {
         } catch (IOException e) {
             LOGGER.error("IOException occured disassembling file", e);
         }
+    }
 
+    private static Configuration getCustomConfiguration() throws IOException {
+        var currentDir = new File(".");
+        var files = currentDir.listFiles((dir, name) -> name.equalsIgnoreCase("config.json"));
 
+        Reader configReader;
+        if (files.length > 0) {
+            LOGGER.info("Custom config file found in current directory");
+            configReader = new BufferedReader(new FileReader(files[0]));
+        } else {
+            LOGGER.warn("No custom config file found in current directory, using default config");
+            LOGGER.warn(
+                    "If you would like to use a custom config, copy the default from assets/config/default.json"
+                            + " into the directory containing this jar file, and rename it to config.json");
+
+            var configStream = Main.class.getResourceAsStream("/assets/config/default.json");
+            configReader = new BufferedReader(new InputStreamReader(configStream));
+        }
+        return new JsonConfiguration().load(configReader);
     }
 
     private static void printHelp() {
